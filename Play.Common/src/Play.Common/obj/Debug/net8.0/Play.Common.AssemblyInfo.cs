339e0d40f3bf32df94ee91c9398c965b8b69a9ea
Naama <naama.nalawala@gmail.com>
2025-01-05 19:13:35 +0400

    Added new inventory service

--- Play.Common/src/Play.Common/obj/Debug/net8.0/Play.Common.AssemblyInfo.cs
+++ Play.Common/src/Play.Common/obj/Debug/net8.0/Play.Common.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Play.Common")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7f01d1c8c8b50d4e6d61b142e7b863863683e833")]
 [assembly: System.Reflection.AssemblyProductAttribute("Play.Common")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Play.Common")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ Play.Inventory/src/Play.Inventory.Service/Program.cs
@@ -0,0 +1,33 @@
+using MongoDB.Driver;
+using Play.Common.MongoDB;
+using Play.Inventory.Service.Entities;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
+builder.Services.AddMongo()
+.AddMongoRepository<InventoryItem>("inventoryitems");
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/swagger/v1/swagger.json", "API v1");
+        options.RoutePrefix = string.Empty; // Make Swagger UI default
+    });
+}
+
+app.UseHttpsRedirection();
+app.MapControllers();
+app.Run();
+
